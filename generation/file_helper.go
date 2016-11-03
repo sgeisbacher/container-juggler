ad7a68fdc71c804270185aa7ce5d78714efb86a9
Stefan Geisbacher <s.geisbacher@netconomy.net>
2016-11-03 07:43:01 +0100

    added implementations of TemplateLoader- and FileHelper-interfaces

---
+++ generation/file_helper.go
@@ -0,0 +1,13 @@
+package generation
+
+import "io/ioutil"
+
+type DefaultFileHelper struct{}
+
+func (fh DefaultFileHelper) Exists(path string) bool {
+	return true
+}
+
+func (fh DefaultFileHelper) Write(path string, data string) error {
+	return ioutil.WriteFile(path, data, 0644)
+}
---
+++ generation/template_loader.go
@@ -0,0 +1,25 @@
+package generation
+
+import (
+	"io/ioutil"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type DefaultTemplateLoader struct{}
+
+func (tl DefaultTemplateLoader) Load(path string) (map[string]interface{}, error) {
+	text, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	tmplYaml := make(map[string]interface{})
+	err = yaml.Unmarshal(text, &tmplYaml)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tmplYaml, nil
+}
